dd273c96a4ff136b2d0495cb6dcbab50e334062e
dca-b <117448+dca-b@users.noreply.github.com>
2024-07-19 15:15:29 +0700

    Update options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,6 +3,6 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.number = false
-vim.opt.relativenumber = false
-vim.opt.signcolumn = "no"
+--vim.opt.number = false
+--vim.opt.relativenumber = false
+--vim.opt.signcolumn = "no"
